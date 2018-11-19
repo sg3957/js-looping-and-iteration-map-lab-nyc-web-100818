652f47dfc51622955dcd51b9cc4269683cd2dead
sg3957 <sg3957@nyu.edu>
2018-11-18 20:24:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file.
+function lowerCaseDrivers(driversArr){
+  return driversArr.map(function(driver){
+    return driver.toLowerCase();
+  });
+
+}
+
+
+function nameToAttributes(driversArr) {
+  return driversArr.map(function(driver) {
+    const firstName = driver.split(' ')[0];
+    const lastName = driver.split(' ')[1];
+
+    return { firstName: firstName, lastName: lastName };
+  });
+
+}
+
+
+function attributesToPhrase(driversArr) {
+  return driversArr.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+}
